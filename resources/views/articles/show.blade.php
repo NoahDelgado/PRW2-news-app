301d9206b644e15a4dca9bb4a8980d6fbe4b3313
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-01-19 14:34:20 +0100

    Add auction sectionin show article

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -10,8 +10,12 @@
         @method('DELETE')
         <input type="submit" value="Supprimer l'article">
     </form>
-
-    <a href="{{ route('articles.edit', $article) }}">Modifier cet article</a>
+    <a href="{{ route('articles.edit', $article) }}">Modifier cet
+        article</a>
+    <h3>Enchère </h3>
+    <p>Date de la meilleur enchère : {{ $article->auctions->created_at }}</p>
+    <a href="{{ route('articles.auctions.create', $article) }}">Enchérir sur cet
+        article</a>
 
     <h3>Commentaires</h3>
 
@@ -24,11 +28,10 @@
     @endunless
 
     <ul>
-    @foreach ($article->comments as $comment)
-        <li>
-            <p>{{ $comment->body  }}</p>
-        </li>
-    @endforeach
+        @foreach ($article->comments as $comment)
+            <li>
+                <p>{{ $comment->body }}</p>
+            </li>
+        @endforeach
     </ul>
-
 @endsection
